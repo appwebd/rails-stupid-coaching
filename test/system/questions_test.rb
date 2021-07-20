f97877916af3caf2d98581f1566e4d7e8e9c7a39
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-07-20 17:58:18 -0400

    was added title in ask and answer views.

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,14 +1,40 @@
-# test/system/questions_test.rb
-require "application_system_test_case"
+require 'application_system_test_case'
+
+QUESTION_WORK = 'I am going to work'
+ANSWER_WORK = 'Great!'
+ANSWER_QUESTION = 'Silly question, get dressed and go to work!'
+ANSWER_ELSE = "I don't care, get dressed and go to work!"
 
 class QuestionsTest < ApplicationSystemTestCase
-  # [...]
-
-  test "saying Hello yields a grumpy response from the coach" do
+  test 'visiting /ask renders the form' do
     visit ask_url
-    fill_in "question", with: "Hello"
-    click_on "Ask"
+    assert_selector 'p', text: 'Ask your coach anything'
+  end
 
-    assert_text "I don't care, get dressed and go to work!"
+  test 'saying `Hello` yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text ANSWER_ELSE
+    take_screenshot
+  end
+
+  test 'asking something or nothing yields a dismissive response from the coach' do
+    visit ask_url
+    fill_in 'question', with: '?'
+    click_on 'Ask'
+
+    assert_text ANSWER_QUESTION
+    take_screenshot
+  end
+
+  test 'saying `I am going to work` yields `Great!` as an answer from the coach' do
+    visit ask_url
+    fill_in 'question', with: QUESTION_WORK
+    click_on 'Ask'
+
+    assert_text ANSWER_WORK
+    take_screenshot
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    fixtures :all
+  end
 end
 
 Capybara.register_driver :headless_chrome do |app|
