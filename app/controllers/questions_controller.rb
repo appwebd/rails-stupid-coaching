d13fc2e5c794c95bf372c8a6dcf0d2325a5da9ab
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-07-23 10:02:50 -0400

    Updated rubocop style of rails-stupid-coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,16 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
-    if @question.blank?
-      @answer = "I can't hear you!"
-    elsif @question =~ /i am going to work/i
-      @answer = "Great!"
-    elsif @question.ends_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @question.blank?
+                "I can't hear you!"
+              elsif @question =~ /i am going to work/i
+                'Great!'
+              elsif @question.ends_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
